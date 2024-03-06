b99f5b0ea1e83096596c4406d2ca54e6d8ed8033
Jason Cain <118203097+cainjason@users.noreply.github.com>
2024-03-06 18:27:40 -0500

    Added main elements

---
+++ app/components/Header.tsx
@@ -0,0 +1,10 @@
+
+function Header() {
+  return (
+    <header>
+      <h2>GAME</h2>
+    </header>
+  )
+}
+
+export default Header
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <main className="">
       Let's build Grant
+      <Header />
+      <Navbar />
+      <Footer />
     </main>
   );
 }
