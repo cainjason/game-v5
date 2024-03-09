2f6301ad546b8e1cf9768d1942a67a7d1fb46437
Jason Cain <118203097+cainjason@users.noreply.github.com>
2024-03-09 10:37:14 -0500

    change: up dated app

--- app/components/Comment.tsx
+++ app/components/Comment.tsx
@@ -6,7 +6,7 @@ function Comment() {
         <input 
             className="text-sm outline-none border-red-600 bg-green-200 pl-2 font-bold" 
             type="text" 
-            placeholder="Where we going?"/>
+            placeholder="Where are we going today?"/>
 
             <SearchIcon />
     </div>
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,15 +1,15 @@
 import ChurchIcon from '@mui/icons-material/Church';
 import MovieIcon from '@mui/icons-material/Movie';
-import Link from 'next/link';
+
 
 function Header() {
   return (
     <header className='shadow-lg shadow-cyan-500/50 flex items-center justify-around bg-gradient-to-r from-blue-500'>
       
-      <MovieIcon className='cursor-pointer hover:bg-blue-200 flex justify-end mr-5  ml-2 h-9 w-9' />
+      <MovieIcon className='cursor-pointer hover:shadow-lg shadow-black-700/50 flex justify-end mr-5  ml-2 h-9 w-9 hover:h-10 hover:w-10' />
       
       <h1 className='flex text-white text-5xl font-extrabold justify-center'>* G~A~M~E *</h1>
-      <ChurchIcon className='cursor-pointer hover:bg-blue-200 flex justify-end mr-5  ml-2 h-9 w-9' />
+      <ChurchIcon className='cursor-pointer hover:shadow-lg shadow-black-700/50 flex justify-end mr-5  ml-2 h-9 w-9 hover:h-10 hover:w-10' />
     </header>
   )
 }
