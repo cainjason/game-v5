4ff5bbef6d1fb6d803ce41b3c30763517ab6d6e6
Jason Cain <118203097+cainjason@users.noreply.github.com>
2024-03-09 01:06:22 -0500

    added a fixed footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,10 +1,15 @@
 
 function Footer() {
   return (
-    <footer>
-      <h2>Footer</h2>
+    <footer className="fixed bottom-0 bg-blue-200 text-xl font-bold">
+      <div className="flex flex-row justify-between ml-5 mr-5">
+        <p className="text-red-700 hover:text-pink-300 cursor-pointer">© 2023 Grant A.M.E. All Rights Reserved.</p>
+        <p className="text-red-700 hover:text-pink-300 cursor-pointer">1906 Washington St. Boston, MA 02118</p>
+        <p>Contact: (617) 427-0670</p>
+        <p className="text-red-700 hover:text-pink-300 cursor-pointer"><a>Info.game.bos@gmail.com</a></p>
+      </div>
     </footer>
   )
 }
 
-export default Footer
+export default Footer;
