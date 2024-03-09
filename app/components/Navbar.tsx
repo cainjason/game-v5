c5658508641069c2a68ca94f77aa2f240bd93087
Jason Cain <118203097+cainjason@users.noreply.github.com>
2024-03-09 01:21:52 -0500

    updated app

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,14 +1,14 @@
 
 function Navbar() {
   return (
-    <nav className="flex flex-col h-full bg-red-200 text-xl font-bold p-2">
-      <p className="text-sky-500 hover:text-sky-700"><a href="#home">Home</a></p>
-      <p className="text-sky-500 hover:text-sky-700"><a href="#calendar">Calendar</a></p>
-      <p className="text-sky-500 hover:text-sky-700"><a href="#ministries">Ministries</a></p>
-      <p className="text-sky-500 hover:text-sky-700"><a href="#infomation">Infomation</a></p>
-      <p className="text-sky-500 hover:text-sky-700"><a href="#contacts">Contacts</a></p>
-      <p className="text-sky-500 hover:text-sky-700"><a href="#giving">Giving</a></p>
-      <p className="text-sky-500 hover:text-sky-700"><a href="#media">Media</a></p>
+    <nav className="flex flex-col h-full w-8 bg-red-200 text-3xl font-bold p-2">
+      <p className="text-sky-500 hover:text-sky-700 mt-5 mb-5"><a href="#home">Home</a></p>
+      <p className="text-sky-500 hover:text-sky-700 mt-5 mb-5"><a href="#calendar">Calendar</a></p>
+      <p className="text-sky-500 hover:text-sky-700 mt-5 mb-5"><a href="#ministries">Ministries</a></p>
+      <p className="text-sky-500 hover:text-sky-700 mt-5 mb-5"><a href="#infomation">Infomation</a></p>
+      <p className="text-sky-500 hover:text-sky-700 mt-5 mb-5"><a href="#contacts">Contacts</a></p>
+      <p className="text-sky-500 hover:text-sky-700 mt-5 mb-5"><a href="#giving">Giving</a></p>
+      <p className="text-sky-500 hover:text-sky-700 mt-5 mb-5"><a href="#media">Media</a></p>
     </nav>
   )
 }
