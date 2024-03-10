2f6777fcc34d2bc4bc783be579406cc6d68f6d70
Jason Cain <118203097+cainjason@users.noreply.github.com>
2024-03-10 08:10:16 -0400

    inlarged svg

--- app/components/Body.tsx
+++ app/components/Body.tsx
@@ -1,17 +1,9 @@
-import Image from 'next/image';
-import NewGrant from '../public/newgrant.png';
 
-function Body() {
-  
-  
+
+function Body () {  
   return (
     <body className="h-full w-full">
-			{/*<Image 
-        src={NewGrant} 
-        alt='church' 
-        width='500px' 
-        height='600px' 
-        placeholder='blur' />*/}
+      
     </body>
   )
 }
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,12 +4,12 @@ import MovieIcon from '@mui/icons-material/Movie';
 
 function Header() {
   return (
-    <header className='shadow-lg shadow-cyan-500/50 flex items-center justify-around bg-gradient-to-r from-blue-500'>
+    <header className=' shadow-lg shadow-cyan-500/50 flex items-center justify-around bg-gradient-to-r from-blue-500'>
       
-      <MovieIcon className='cursor-pointer hover:shadow-lg shadow-black-700/50 flex justify-end mr-5  ml-2 h-9 w-9 hover:h-10 hover:w-10' />
+      <MovieIcon className='cursor-pointer hover:shadow-lg shadow-black-700/50 flex justify-end mr-5  ml-2 h-40 w-40 hover:h-40 hover:w-40' />
       
       <h1 className='flex text-white text-5xl font-extrabold justify-center'>* G~A~M~E *</h1>
-      <ChurchIcon className='cursor-pointer hover:shadow-lg shadow-black-700/50 flex justify-end mr-5  ml-2 h-9 w-9 hover:h-10 hover:w-10' />
+      <ChurchIcon className='cursor-pointer hover:shadow-lg shadow-black-700/50 flex justify-end mr-5  ml-2 h-40 w-40 hover:h-40 hover:w-40' />
     </header>
   )
 }
